67d2bdaf97eae189f79ef1b55ee8f305956583b3
ugtan <umang.taneja0173@gmail.com>
2018-06-21 20:38:12 +0530

    Updated FideStats

--- FideStats.py
+++ FideStats.py
@@ -3,56 +3,63 @@
 import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
+import pandas as pd
 
 Choice ={
           "1": "Fide Events",
           "2": "Tournaments",
           "3": "Ratings"
-        }
+}
 
+Events = {
+            "0": "World Events",
+            "1": "European Events",
+            "2": "American Zonals",
+            "3": "American Events",
+            "4": "Asian Events",
+            "5": "African Zonals",
+            "6": "African Events",
+            "7": "Arab Events",
+            "8": "Presidential Board / Congress",
+            "9": "Other Official Events"
+
+}
 Categories = {
-                "1": "Top 100 Players",
-                "2": "Top 100 Women",
-                "3": "Top 100 Juniors",
-                "4": "Top 100 Girls"
+                "1": "Top 20 Players",
+                "2": "Top 20 Women",
+                "3": "Top 20 Juniors",
+                "4": "Top 20 Girls"
              }
-Modes = {
+
+Variant = {
            "1": "Standard",
            "2": "Rapid",
            "3": "blitz"
         }
 
-def scrape(url):
-    """ To fetch info from the urls """
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text , "html.parser")
-    return soup
 
-def fide_events(soup):
+def fide_events(soup, event):
     """ To Fetch Fide Events from fide_events"""
     headers = ["Name", "Place", "Start", "End"]
+    print("\n\t" + event + "\n")
+    data = []
+    my_data =[]
 
     tables = soup.find_all("table",{"align":"center"})[1:]
     for table in tables:
-        data = []
-        my_data =[]
-        trs = table.find_all("tr")[0]
-        td = trs.find("td")
-        print(td.text)
-        tr_tags = table.find_all("tr")[2:]
-        for tr_tag in tr_tags:
-            td_tags = tr_tag.find_all("td")
-            for td in td_tags:
-                data.append(td.text)
-
-        my_data = [data[x:x+4] for x in range(0, len(data),4)]
-        if my_data != [] :
+        if str(table.find_all("tr")[0].find("td").text).strip() == str(event):
+            tr_tags = table.find_all("tr")[2:]
+            for tr_tag in tr_tags:
+                td_tags = tr_tag.find_all("td")
+                for td in td_tags:
+                    data.append(td.text)
+            my_data = [data[x:x+4] for x in range(0, len(data),4)]
             print(tabulate(my_data,headers=headers,tablefmt = "fancy_grid"))
-        else:
             break
+
+
 def tournaments(soup):
     """" To Fetch tournament info from the soup object """
-
     data = []
     my_data =[]
     headers = ["Event Name", "City","Federation","Start", "End"]
@@ -67,15 +74,21 @@ def tournaments(soup):
     print(tabulate(my_data,headers=headers,tablefmt = "fancy_grid"))
 
 def ratings(soup):
+    """ To get the ratings of top players """
     data = []
     my_data =[]
     headers = ["Rank", "Name"]
-    #headers = ["Rank", "Name", "Title","Country","Rating", "Games", "B-Year"]
     atags = soup.find_all("a", {"class":"tur"})
     for count,atag in enumerate(atags,1):
-        data.append([count,atag.text])
-
+        if len(data) != 20:
+            data.append([count,atag.text])
     print(tabulate(data,headers=headers,tablefmt = "fancy_grid"))
+
+def scrape(url):
+    """ To fetch info from the urls """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text , "html.parser")
+    return soup
 
 
 def f(catg):
@@ -87,19 +100,27 @@ def f(catg):
             '4': 'girls'
     }[catg]
 
+
 def main():
 
-    print(Choice)
-    choice = input("Enter your choice!")
+    for i, j in sorted(Choice.items()):
+        print(i, j)
 
-    while choice not in ["1","2","3"]:
-        print("Wrong choice! Please enter a valid choice!")
-        choice = input("Enter your choice!")
+    choice = input("\nEnter your choice: ")
+
+    while choice not in map(str, range(4)):
+        choice = input("\nWrong choice! Please enter a valid choice! ")
 
     if choice == "1":
         url = 'https://www.fide.com/calendar/fide-calendar.html'
         soup = scrape(url)
-        fide_events(soup)
+        for i, j in sorted(Events.items()):
+            print(i, j)
+        event = input("\nEnter your choice! ")
+
+        while event not in map(str, range(10)):
+            event = input("\nWrong choice! Please enter a valid choice! ")
+        fide_events(soup,Events[event])
 
     if choice == "2":
         url = 'https://ratings.fide.com/tournament_list.phtml'
@@ -107,26 +128,27 @@ def main():
         tournaments(soup)
 
     if choice == "3":
-        print(Modes)
-        mode = input("Enter your choice!")
+        for i, j in sorted(Variant.items()):
+            print(i, j)
 
-        while mode not in ["1","2","3"]:
-            print("Wrong choice! Please enter a valid choice!")
-            mode = input("Enter your choice!")
-        print(Categories)
-        catg = input("Enter your choice!")
+        variant = input("\nEnter your choice! ")
+        while variant not in map(str, range(4)):
+            variant = input("\nWrong choice! Please enter a valid choice! ")
 
-        while catg not in ["1","2","3","4"]:
-            print("Wrong choice! Please enter a valid choice!")
-            catg = input("Enter your choice!")
+        for i, j in sorted(Categories.items()):
+            print(i, j)
+
+        catg = input("\nEnter your choice! ")
+        while catg not in map(str, range(5)):
+            catg = input("\nWrong choice! Please enter a valid choice! ")
 
         string = ""
 
-        if mode == "1":
+        if variant == "1":
             string = f(catg)
-        if mode == "2":
+        if variant == "2":
             string = str(f(catg)) + "_rapid"
-        if mode == "3":
+        if variant == "3":
             string = str(f(catg)) + "_blitz"
 
         url = 'https://ratings.fide.com/top.phtml?list={}'.format(string)
